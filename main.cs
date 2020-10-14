4633cb322ee1751db5de40f267881ca8fc6b6222
X-San <70487315+XWasHere@users.noreply.github.com>
2020-10-14 13:07:58 -0400

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,5 +1,14 @@
-class Write {
-public void writeout(a) {
 
+class Write {
+public List<string> getletters(bool returnthing) {
+  
+}
+  
+public void writeout(string a) {
+local string b = a;
+local string c = "";
+local int i = 0;
+local List<string> letters = getletters(!true);
+Console.WriteLine(b);  
 }
 }
