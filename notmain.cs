048c6d9bc684d06ac9e7d68fb3f89aea8fee51aa
X-San <70487315+XWasHere@users.noreply.github.com>
2020-10-14 13:31:49 -0400

    Update notmain.cs

--- notmain.cs
+++ notmain.cs
@@ -1,6 +1,60 @@
-using write;
+namespace write {
+public class Write {
+public List<string> getletters(bool returnthing) {
+  local List<string> l = new List<string>();
+  l[0]=@"a";
+  l[1]=@"b";
+  l[2]=@"c";
+  l[3]=@"d";
+  l[4]=@"e";
+  l[5]=@"f";
+  l[6]=@"g";
+  l[7]=@"h";
+  l[8]=@"i";
+  l[9]=@"j";
+    l[10]=@"k";
+    l[11]=@"l";
+  l[12]=@"m";
+  l[13]=@"n";
+  l[14]=@"o";
+  l[15]=@"p";
+  l[16]=@"q";
+  l[17]=@"r";
+  l[18]=@"s";
+  l[19]=@"t";
+  l[20]=@"u";
+  l[21]=@"v";
+  l[22]=@"x";
+  l[23]=@"y";
+  l[24]=@"and";
+  l[25]=@"z";
+  l[26]=@"now";
+  l[27]=@"i";
+  l[28]=@"know";
+  l[29]=@"my";
+  l[30]=@"abcs";
+  l[31]=@"next";
+    l[32]=@"time";
+    l[33]=@"won't";
+    l[34]=@"you";
+    l[35]=@"sing";
+    l[36]=@"with";
+    l[37]=@"me";
+    return l;
+  }
+  
+  public void writeout(string a) {
+    local string b = a;
+    local string c = "";
+    local int i = 0;
+    local List<string> letters = getletters(!true);
+    Console.WriteLine(b);  
+  }
+}
+}
+
 class oh {
 static void main() {
-write.Write("a");
+write.Write("Hello World");
 }
 }
